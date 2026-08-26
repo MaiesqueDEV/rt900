b7b5cd523546f52d9e9b1af6a0ac0fccc9307e97
MaiesqueDEV <maiesquedev@gmail.com>
2026-08-26 12:37:55 -0300

    Personalizacao da tela de abertura para MAIESQUE

--- Gui/DisplayPowerOn.c
+++ Gui/DisplayPowerOn.c
@@ -2,30 +2,19 @@
 
 extern void UI_DisplayPowerOn(void)
 {
-    U8 i;
-    U8 posX,posY;
-    U32 addr;
-    U8 picDisBuf[2560+1] = {0};
-
-
-    if(g_radioInform.OpFlag1.Bit.b0 == 0)
-    {
-        addr = FLASH_PON_MSG_ADDR;
-        for(i=0;i<(LCD_HEIGHT/8);i++)
-        {
-            SpiFlash_ReadBytes(addr,picDisBuf,2560);
-            ST7735S_DisplayColorArea(posX,posY,LCD_WIDTH, 8, picDisBuf);
-            posY += 8;
-            addr += 2560;
-        }
-    }
-    else
-    {
-        DisplayBatteryVol(0);
-    }
-
+    // Limpa a tela com fundo preto (0x0000)
+    LCD_ClearArea(0, 0, 160, 128, 0x0000);
     LcdBackLightSwitch(LED_ON);
-    //Delay 500ms
-    DelayMs(500);
-}
 
+    // Exibe "MAIESQUE" em destaque grande (Amarelo 0xFFE0 em fundo Preto 0x0000)
+    LCD_DisplayText(48, 35, (U8 *)"MAIESQUE", FONTSIZE_16x16, 0xFFE0, 0x0000, 0);
+
+    // Exibe "BINTOLK RT900" (Ciano 0x07FF)
+    LCD_DisplayText(40, 65, (U8 *)"BINTOLK RT900", FONTSIZE_6x12, 0x07FF, 0x0000, 0);
+
+    // Exibe "CUSTOM FIRMWARE" (Verde 0x07E0)
+    LCD_DisplayText(34, 85, (U8 *)"CUSTOM FIRMWARE", FONTSIZE_6x12, 0x07E0, 0x0000, 0);
+
+    // Mantém a mensagem de abertura na tela por 1.5 segundos ao ligar
+    DelayMs(1500);
+}
